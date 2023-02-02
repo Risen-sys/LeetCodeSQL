78a049dab1d6355e600380bbbc4d0e1553261113
Matthew Silbernagel <118539484+Risen-sys@users.noreply.github.com>
2023-02-02 11:16:30 -0600

    Update LeetCodeSQL-DepartmentTop3Salaries.sql

--- LeetCodeSQL-DepartmentTop3Salaries.sql
+++ LeetCodeSQL-DepartmentTop3Salaries.sql
@@ -1,4 +1,7 @@
-select d.name as Department, e1.name as Employee, e1.salary as Salary from employee e1 join department d on e1.departmentId=d.id
-where salary in ( select distinct top 3 e2.salary
-from employee e2 where e1.departmentId=e2.departmentId
-order by e2.salary desc )
+SELECT  d.name    AS Department
+       ,e1.name   AS Employee
+       ,e1.salary AS Salary
+FROM employee e1
+JOIN department d
+ON e1.departmentId = d.id
+WHERE salary IN ( SELECT distinct top 3 e2.salary FROM employee e2 WHERE e1.departmentId = e2.departmentId ORDER BY e2.salary desc )
